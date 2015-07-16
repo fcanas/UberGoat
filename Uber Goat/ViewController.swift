80d5925f9350d8813f7023beac75b51504d00b23
Fabian Canas <fcanas@gmail.com>
2015-07-16 15:47:19 -0400

    Initial prototype

---
+++ Uber Goat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Uber Goat
+//
+//  Created by Fabian Canas on 7/16/15.
+//  Copyright © 2015 Fabian Canas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
