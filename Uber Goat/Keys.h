daf786937433527849c61dab70b13b3a21bf4c05
Fabian Canas <fcanas@gmail.com>
2015-07-24 15:03:45 -0400

    Add Parse keys

---
+++ Uber Goat/Keys.h
@@ -0,0 +1,12 @@
+//
+//  Keys.h
+//  Uber Goat
+//
+//  Created by Fabian Canas on 7/24/15.
+//  Copyright © 2015 Fabian Canas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString * const ParseAppID;
+extern NSString * const ParseAppKey;
---
+++ Uber Goat/Keys.m
@@ -0,0 +1,13 @@
+
+//
+//  Keys.m
+//  Uber Goat
+//
+//  Created by Fabian Canas on 7/24/15.
+//  Copyright © 2015 Fabian Canas. All rights reserved.
+//
+
+#import "Keys.h"
+
+NSString * const ParseAppID = PARSE_APPLICATION_ID;
+NSString * const ParseAppKey = PARSE_CLIENT_KEY;
